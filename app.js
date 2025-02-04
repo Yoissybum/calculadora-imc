566770c2a18c6c020ea8553a9cc44ac491da7cf5
Yoissy Egusquiza <yoissy.egusquizall@gmail.com>
2025-02-03 23:30:45 -0500

    he cambiado la imagen de sobre peso, y agregue estilos al DOM

--- app.js
+++ app.js
@@ -1,29 +1,22 @@
-// Solicitar el nombre del usuario
 let userName = prompt("Introduce tu nombre, por favor");
 
-// Solicitar el peso del usuario
 let peso = parseFloat(prompt("Introduce tu peso en Kg"));
 
-// Solicitar la altura del usuario
 let altura = parseFloat(prompt("Ahora introduce tu altura en metro"));
 
-// Obtener el contenedor del DOM
 let containerDOM = document.getElementById("container");
 
-// Validar que los datos sean números positivos
 if (isNaN(peso) || peso <= 0) {
     alert("Por favor, introduce un peso válido.");
 } else if (isNaN(altura) || altura <= 0) {
     alert("Por favor, introduce una altura válida.");
 } else {
-    // Calcular el IMC
+  
     let imc = peso / (altura * altura);
-    
-    // Mostrar el resultado
     let mensaje;
     let imagen;
 
-    // Interpretar el resultado
+
     if (imc < 18.5) {
         mensaje = `¡Ten cuidado, ${userName}! Tienes bajo peso.`;
         imagen = "https://cdn-icons-png.flaticon.com/512/5559/5559962.png";
@@ -38,11 +31,10 @@ if (isNaN(peso) || peso <= 0) {
         containerDOM.style.backgroundColor = "#ffcc00"; 
     } else {
         mensaje = `¡Oh, no, ${userName}! Por favor cuida tu salud, ya llegaste a la obesidad.`;
-        imagen = "https://img.freepik.com/vector-gratis/problema-obesidad-diagnostico-consulta-medica-hombre-sobrepeso-impacto-negativo-obesidad-salud-humana-organos-internos-ilustracion-metafora-concepto-aislado-vector_335657-1305.jpg";
+        imagen = "https://especiales.publimetro.co/famisanar-obesidad/wp-content/uploads/2022/12/Recurso-1.png";
         containerDOM.style.backgroundColor = "#ff4c4c"; 
     }
 
-    // Mostrar el mensaje y la imagen en el contenedor
     containerDOM.innerHTML = `
         <img src="${imagen}" alt="Estado de peso">
         <h1>${mensaje}</h1>
@@ -50,6 +42,7 @@ if (isNaN(peso) || peso <= 0) {
     `;
     containerDOM.style.textAlign = "center"
     containerDOM.style.fontFamily = "Arial"
+    containerDOM.style.padding = "50px"
     mensaje.style.fontFamily = "Arial"
 }
 
